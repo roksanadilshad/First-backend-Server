098bbb0d4ddcd0d9cd82c89f8d442f37286ef564
Roksana Dilshad <roksanadilshad@gmail.com>
2025-10-31 17:13:31 +0600

    Add Functionality

--- Index.js
+++ Index.js
@@ -1,12 +1,69 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const port = 5000;
+const { MongoClient, ServerApiVersion, ProfilingLevel, ObjectId } = require('mongodb');
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.send('my first backend file..')
-})
+app.use(cors());
+app.use(express.json());
 
-app.listen(port, ()=>{
-    console.log(`my server is running on port : ${port}`)
+const uri = "mongodb+srv://databaseMeeUser:EmXwoX2SucVPIvBh@clusterok.helsitf.mongodb.net/?appName=Clusterok";
+
+//EmXwoX2SucVPIvBh
+//databaseMeeUser
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
+app.get('/', (req, res)=>{
+    res.send('hi i am ruklu')
+})
+
+async function run() {
+    try{
+        await client.connect();
+         const usersDB = client.db('usersDB');
+         const usersCollection = usersDB.collection('users')
+
+         app.get('/users', async (req, res)=>{
+          const cursor = usersCollection.find();
+          const result = await cursor.toArray();
+          res.send(result);
+         })
+
+        //add database related api here 
+        app.post('/users', async (req, res)=>{
+          const newUser = req.body;
+          const result = await usersCollection.insertOne(newUser);
+          res.send(result)
+        })
+
+        app.delete('/users/:id', async (req, res)=>{
+          console.log(req.params.id);
+          const id = req.params.id;
+          const query =  {_id: new ObjectId(id)};
+          const result = await usersCollection.deleteOne(query);
+          res.send(result)
+          
+          console.log('delete a user from database');
+          
+        })
+        await client.db('admin').command({ ping: 1})
+        console.log('pinged your deployedment');
+        
+    }
+    finally{
+
+    }
+    
+}
+run().catch(console.dir)
+
+
+app.listen(port, ()=>{
+    console.log(`happy to see you ${port}`)
+})
